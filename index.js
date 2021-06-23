f15c8e4c0bc69329aa7c16f1c7e269512ee37e7c
Triana Cerda <trianaRcerda@gmail.com>
2021-06-23 16:46:02 -0700

    name tag functional edits

--- index.js
+++ index.js
@@ -1,49 +1,24 @@
 console.log("hello world!");
+
 const nameTag = document.getElementById('name-tag');
 console.log(nameTag)
 
-//const nameSection = document.getElementById('name-section');
-//nameSection.addEventListener('click', ()=>{
-    //console.log("I am clicking")
-    //nameSection.innerText = "Jon";
-//})
-
-//const topSection = document.getElementById ('top-section');
-//topSection.style.backgroundColor = "purple";
-
-//const bottomSection = document.getElementById ('bottom-section');
-//bottomSection.style.backgroundColor = "yellow";
+const nameSection = document.getElementById('name-section');
+console.log(nameSection);
 
 const nameInput = document.getElementById('name-input');
-nameInput.addEventListener('input', (e)=>{
-    console.log(e.target.value)
-    nameSection.innerText = e.target.value;
-})
 
 const submitButton =document.getElementById('submit')
-submitButton.addEventListener.toString('click', (e)=>{
+submitButton.addEventListener('click', (e)=>{
     nameSection.innerText = nameInput.value;
 })
 
 const pronounsInput = document.getElementById('pronouns-input');
-pronounsInput.addEventListener('input', (e)=>{
-    //console.log(e.target.value)
-    pronounsSection.innerText = e.target.value;
-})
 
 const pronounsSection = document.getElementById('pronouns-section') 
 
 const pronounsSubmit = document.getElementById('pronouns-submit')
-pronounsSubmit.addEventListener.toString('click', (e)=>{
+pronounsSubmit.addEventListener('click', (e)=>{
     pronounsSection.innerText = pronounsInput.value;
 })
 
-
-
-
-
-
-//function shout(name){
-    //console.log(name.toUpperCase());
-//}
-//shout('Triana');
