9ee0520685d087066509c23c19bbc34997bbe637
Triana Cerda <trianaRcerda@gmail.com>
2021-06-23 15:09:25 -0700

    added buttons for name tag

--- index.js
+++ index.js
@@ -2,17 +2,17 @@ console.log("hello world!");
 const nameTag = document.getElementById('name-tag');
 console.log(nameTag)
 
-const nameSection = document.getElementById('name-section');
-nameSection.addEventListener('click', ()=>{
-    console.log("I am clicking")
-    nameSection.innerText = "Jon";
-})
+//const nameSection = document.getElementById('name-section');
+//nameSection.addEventListener('click', ()=>{
+    //console.log("I am clicking")
+    //nameSection.innerText = "Jon";
+//})
 
-const topSection = document.getElementById ('top-section');
-topSection.style.backgroundColor = "purple";
+//const topSection = document.getElementById ('top-section');
+//topSection.style.backgroundColor = "purple";
 
-const bottomSection = document.getElementById ('bottom-section');
-bottomSection.style.backgroundColor = "yellow";
+//const bottomSection = document.getElementById ('bottom-section');
+//bottomSection.style.backgroundColor = "yellow";
 
 const nameInput = document.getElementById('name-input');
 nameInput.addEventListener('input', (e)=>{
@@ -25,11 +25,25 @@ submitButton.addEventListener.toString('click', (e)=>{
     nameSection.innerText = nameInput.value;
 })
 
+const pronounsInput = document.getElementById('pronouns-input');
+pronounsInput.addEventListener('input', (e)=>{
+    //console.log(e.target.value)
+    pronounsSection.innerText = e.target.value;
+})
+
+const pronounsSection = document.getElementById('pronouns-section') 
+
+const pronounsSubmit = document.getElementById('pronouns-submit')
+pronounsSubmit.addEventListener.toString('click', (e)=>{
+    pronounsSection.innerText = pronounsInput.value;
+})
 
 
-function shout(name){
-    console.log(name.toUpperCase());
 
-}
 
-shout('Triana');
+
+
+//function shout(name){
+    //console.log(name.toUpperCase());
+//}
+//shout('Triana');
